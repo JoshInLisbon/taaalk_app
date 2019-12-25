2a06cbe4d9deed9ee0d846f134e543d26eb778ff
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2019-12-25 00:17:00 +0000

    something?

--- app/controllers/tlks_controller.rb
+++ app/controllers/tlks_controller.rb
@@ -8,7 +8,6 @@ class TlksController < ApplicationController
   def show
     @tlk = Tlk.includes(:participants, :messages).find(params[:id])
     @user_participants = Participant.where(tlk: @tlk, user: current_user)
-    # pundit if statment?
     @message = Message.new()
   end
 end
